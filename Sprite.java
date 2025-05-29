44284f2b63a84de9472cc7315a0728dc7936c1c0
J.A.Bianchi <JABianchi@gmail.com>
2025-05-29 01:33:21 -0400

    updated javadoc formatting

--- Sprite.java
+++ Sprite.java
@@ -1,8 +1,11 @@
-/* Sprite class - to create objects that move around with their own properties
+/** 
+ * Sprite class - to create objects that move around with their own properties
  * Inspired by Daniel Shiffman's p5js Animated Sprite tutorial
  * Note: Picture coordinate origina at top, left corner
- * Author: Joel Bianchi & Marcus Bistline
- * Last Edit: 5/28/25
+ * @author Joel Bianchi
+ * @author Marcus Bistline
+ * @version 5/29/25
+ * updated formatting for javadoc
  * Added isOverlapping() method
  * Added isTouchingTop() method
  * Added isSolid property to Sprites
@@ -39,22 +42,46 @@ public class Sprite{
 
   //------------------ SPRITE CONSTRUCTORS --------------------//
 
-  // Sprite Constructor #1: Only pass in the image file (Non-animated)
+  /**
+   * Sprite Constructor #1: Only pass in the image file (Non-animated)
+   * @param p             Processing applet
+   * @param spriteImgFile filename for the non-animated sprite
+   */
   public Sprite(PApplet p, String spriteImgFile){
     this(p, spriteImgFile, 1.0f, 0.0f, 0.0f, false);
   }
 
-  // Sprite Constructor #2: Only pass in the image file that can be scaled (Non-animated)
+  /**
+   * Sprite Constructor #2: Only pass in the image file that can be scaled (Non-animated)
+   * @param p             Processing applet
+   * @param spriteImgFile filename for the non-animated sprite
+   * @param scale         float that multiplies the size of the image to display
+   */
   public Sprite(PApplet p, String spriteImgFile, float scale){
     this(p, spriteImgFile, scale, 0.0f, 0.0f, false);
   }
 
-  // Sprite Constructor #3: for Non-Animated Sprite (not working)
+  /**
+   * Sprite Constructor #3: for Non-Animated Sprite (not working)
+   * @param p             Processing applet
+   * @param spriteImgFile filename for the non-animated sprite
+   * @param scale         float that multiplies the size of the image to display
+   * @param x             sets the initial LEFT edge of the Sprite
+   * @param y             sets the initial TOP edge of the Sprite
+   */
   public Sprite(PApplet p, String spriteImgFile, float scale, float x, float y) {
     this(p, spriteImgFile, scale, x, y, false);
   }
 
-  // Sprite Constructor #4: for ANY Sprite from a file name
+  /**
+   * Sprite Constructor #4: for ANY Sprite from a file name
+   * @param p             Processing applet
+   * @param spriteImgFile filename for the non-animated sprite
+   * @param scale         float that multiplies the size of the image to display
+   * @param x             sets the initial LEFT edge of the Sprite
+   * @param y             sets the initial TOP edge of the Sprite
+   * @param isAnimated    true if animated, false otherwise
+   */
   public Sprite(PApplet p, String spriteImgFile, float scale, float x, float y, boolean isAnimated) {
 
     System.out.println("Sprite: Loading new Sprite " + spriteImgFile + " which is animated? " + isAnimated);
@@ -83,7 +110,14 @@ public class Sprite{
 
   }
 
-  // Sprite Constructor #5: Input a PImage directly, Used for moveable Sprites
+  /**
+   * Sprite Constructor #5: Input a PImage directly, Used for moveable Sprites
+   * @param p             Processing applet
+   * @param spriteImg     pre-loaded PImage for Sprite
+   * @param scale         float that multiplies the size of the image to display
+   * @param x             sets the initial LEFT edge of the Sprite
+   * @param y             sets the initial TOP edge of the Sprite
+   */
   public Sprite(PApplet p, PImage spriteImg, float scale, float x, float y) {
 
     System.out.println("Sprite: Loading new moveable Sprite!");
@@ -99,13 +133,21 @@ public class Sprite{
     this.isSolid = true;
   }
 
-  // Sprite Constructor #6: Blob of color Sprite, used for Platform
-  public Sprite(PApplet p, int color, float posXCenter, float posYTop, float platWidth, float platHeight){
+  /**
+   * Sprite Constructor #6: Rectangle of color Sprite, used for Platform
+   * @param p             Processing applet
+   * @param color         PColor int for the color of the rectangle
+   * @param posXCenter    pixel x-value of center of rectangle
+   * @param posYTop       pixel y-value of top of rectangle
+   * @param rectWidth     number of pixels wide of rectangle
+   * @param rectHeight    number of pixels high of rectangle
+   */
+  public Sprite(PApplet p, int color, float posXCenter, float posYTop, float rectWidth, float rectHeight){
 
     System.out.println("Sprite: Loading color-blob Sprite!");
     this.p = p;
-    this.w = platWidth;
-    this.h = platHeight;
+    this.w = rectWidth;
+    this.h = rectHeight;
     setCenterX(posXCenter);
     setTop(posYTop);
     this.speedX = 0;
@@ -119,7 +161,9 @@ public class Sprite{
 
   //------------------ SPRITE MOTION METHODS --------------------//
 
-  // method to display the Sprite image on the screen
+  /**
+   * Displays the Sprite image on the screen
+   */
   public void show() {
 
     update();
@@ -135,104 +179,157 @@ public class Sprite{
       // System.out.println("spriteshow\tcolor blob");
     }
   }
-
-  // method to move Sprite image on the screen to a specific coordinate
+  
+  /**
+   * Moves Sprite image on the screen to a specific coordinate 
+   * @param x           moves left edge to this pixel value
+   * @param y           moves top edge to this pixel value
+   */
   public void moveTo(float x, float y){
     setLeft(x);
     setTop(y);
   }
-
-  // method to move Sprite image on the screen relative to current position
+  
+  /** 
+   * Moves Sprite image on the screen relative to current position
+   * @param changeX     positive moves right, negative moves left
+   * @param changeY     positive moves down, negative moves up
+   */
   public void move(float changeX, float changeY){
     this.centerX += changeX;
     this.centerY += changeY;
     // System.out.println(getLeft() + "," + getTop());
   }
-
+  
+  /** 
+   * @param speedX
+   */
   public void setSpeedX( float speedX){
     this.speedX = speedX;
   }
-
+  
+  /** 
+   * @param speedY
+   */
   public void setSpeedY( float speedY){
     this.speedY = speedY;
   }
-
-  // Changes the speed of the Sprite
+  
+  /** 
+   * Changes the speed of the Sprite
+   * @param speedX
+   * @param speedY
+   */
   public void setSpeed( float speedX, float speedY){
     setSpeedX(speedX);
     setSpeedY(speedY);
   }
-
-  // Gets the speed of the Sprite in the X-direction
+  
+  /**
+   * Gets the speed of the Sprite in the X-direction 
+   * @return float
+   */
   public float getSpeedX(){
     return speedX;
   }
-
-  // Gets the speed of the Sprite in the Y-direction
+  
+  /** 
+   * Gets the speed of the Sprite in the Y-direction
+   * @return float
+   */
   public float getSpeedY(){
     return speedX;
   }
-
-  // Change the acceleration of the Sprite in the X-direction
+  
+  /** 
+   * Changes the acceleration of the Sprite in the X-direction
+   * @param accelX
+   */
   public void setAccelerationX(float accelX){
     this.accelX = accelX;
   }
   
-  // Change the acceleration of the Sprite in the Y-direction
+  /** 
+   * Changes the acceleration of the Sprite in the Y-direction
+   * @param accelY
+   */
   public void setAccelerationY(float accelY){
     this.accelY = accelY;
   }
 
-  // Starts gravity acting on a Sprite at default rate or previously defined rate
+  /**
+   * Starts gravity acting on a Sprite at default rate or previously defined rate
+   */
   public void startGravity(){
     this.hasGravity = true;
     setAccelerationY(this.gravityStrength);
   }
-
-  // Starts gravity acting on sprite at a particular acceleration rate, 
-  // positive acceleration in Y-direction is downwards
+  
+  /** 
+   * Starts gravity acting on sprite at a particular acceleration rate
+   * @param gravityStrength   positive acceleration in Y-direction is downwards
+   */
   public void startGravity(float gravityStrength){
     this.gravityStrength = gravityStrength;
     startGravity();
   }
 
-
-  // Stops gravity acting on a Sprite
+  /**
+   * Stops gravity acting on a Sprite
+   */
   public void stopGravity(){
     this.hasGravity = false;
     setAccelerationY(0.0f);
   }
-
-  // Creates a jump with a specific jumpSpeed at a pre-existing gravityStrength
+  
+  /** 
+   * Creates a jump with a specific jumpSpeed at a pre-existing gravityStrength
+   * @param jumpSpeed
+   */
   public void jump(float jumpSpeed){
     setSpeedY(-jumpSpeed);
     startGravity();
   }
-
-  // Creates a jump with a specific jumpSpeed and also sets the Sprite's gravity strength
+  
+  /** 
+   * Creates a jump with a specific jumpSpeed and also sets the Sprite's gravity strength
+   * @param jumpSpeed
+   * @param gravityStrength
+   */
   public void jump(float jumpSpeed, float gravityStrength){
     startGravity(gravityStrength);
     jump(jumpSpeed);
   }
 
-  // Creates a jump with default jump speed
+  /**
+   * Creates a jump with default jump speed
+   */
   public void jump(){
     jump(defaultJumpSpeed);
   }
 
-  // Rotates Sprite image on the screen
+  /** 
+   * Rotates Sprite image on the screen
+   * @param degrees
+   */
   public void rotate(float degrees){
     float rads = p.radians(degrees);
     p.translate(centerX,centerY);
     rotate(rads);
   }
-
-  // Changes if a Sprite is solid
+  
+  /** 
+   * Changes if a Sprite is solid
+   * @param isSolid
+   */
   public void setSolid(boolean isSolid){
     this.isSolid = isSolid;
   }
-
-  // Checks if a Sprite is solid
+  
+  /** 
+   * Checks if a Sprite is solid
+   * @return boolean
+   */
   public boolean isSolid(){
     return isSolid;
   }
@@ -240,21 +337,30 @@ public class Sprite{
 
   //------------------ SPRITE COLLISSIONS & OVERLAPS --------------------//
   
-  // Checks if the rectangle of this Sprite overlaps in any way with the rectangle of another
-  public boolean isOverlapping(Sprite other){
-    if(this.getBottom() >= other.getTop()   //bottom isn'too low
-      && this.getTop() <= other.getBottom() //top isn't too high
-      && this.getLeft() <= other.getRight() // left isn't too far right
-      && this.getRight() >= other.getLeft()       // right isn't too far left
+  /** 
+   * Checks if the rectangle of this Sprite overlaps in any way with the rectangle of another
+   * @param otherSprite   second Sprite to compare this Sprite with
+   * @return boolean      true if other Sprit overlaps with this, false otherwise
+   */
+  public boolean isOverlapping(Sprite otherSprite){
+    if(this.getBottom() >= otherSprite.getTop()   //bottom isn'too low
+      && this.getTop() <= otherSprite.getBottom() //top isn't too high
+      && this.getLeft() <= otherSprite.getRight() // left isn't too far right
+      && this.getRight() >= otherSprite.getLeft()       // right isn't too far left
     ){
       return true;
     } else {
       return false;
     }
   }
-
-
-  // Checks if sprite has landed on top of another sprite with a specified cushion on top -Marcus Bistline 2025
+  
+  /** 
+   * Checks if sprite has landed on top of another sprite with a specified cushion on top
+   * @author Marcus Bistline 2025
+   * @param otherSprite   second Sprite to compare this Sprite with
+   * @param cushion       number of pixels above the top before considering the top touched
+   * @return boolean      true if this Sprite is touching the top of the other Sprite, false otherwise
+   */
   public boolean isTouchingTop(Sprite otherSprite, double cushion){
     if(this.getBottom() > otherSprite.getTop()-cushion //is player low enough to touch platform
       && this.getBottom() < otherSprite.getBottom() // is player still above the platform
@@ -265,45 +371,86 @@ public class Sprite{
       return  false;
     }
   }
-
-  // Checks if sprite has landed on top of another sprite
+  
+  /** 
+   * Checks if sprite has landed on top of another sprite
+   * @param otherSprite   second Sprite to compare this Sprite with
+   * @return boolean      true if this Sprite is touching the top of the other Sprite, false otherwise
+   */
   public boolean isTouchingTop(Sprite otherSprite){
     return isTouchingTop(otherSprite, 10);
   }
 
-
-
-
+  
   //------------------ SPRITE COORDINATES ACCESSOR & MUTATOR METHODS --------------------//
-
+  
+  /** 
+   * @return float
+   */
   public float getW(){
     return w;
   }
+  
+  /** 
+   * @return float
+   */
   public float getH(){
     return h;
   }
+  
+  /** 
+   * @return float
+   */
   public float getCenterX(){
     return centerX;
   }
+  
+  /** 
+   * @return float
+   */
   public float getCenterY(){
     return centerY;
   }
+  
+  /** 
+   * @return float
+   */
   public float getX(){
     return getCenterX();
   }
+  
+  /** 
+   * @return float
+   */
   public float getY(){
     return getCenterY();
   }
 
+  
+  /** 
+   * @param w
+   */
   public void setW(float w){
     this.w = w;
   }
+  
+  /** 
+   * @param h
+   */
   public void setH(float h){
     this.h=h;
   }
+  
+  /** 
+   * @param centerX
+   */
   public void setCenterX(float centerX){
     this.centerX = centerX;
   }
+  
+  /** 
+   * @param centerY
+   */
   public void setCenterY(float centerY){
     this.centerY=centerY;
   }
@@ -313,57 +460,110 @@ public class Sprite{
   * -- Used from Long Bao Nguyen
   *  -- https://longbaonguyen.github.io/courses/platformer/platformer.html
   */
+
+  /** 
+   * @param left
+   */
   void setLeft(float left){
     centerX = left + w/2;
   }
+  
+  /** 
+   * @return float
+   */
   float getLeft(){
     return centerX - w/2;
   }
+  
+  /** 
+   * @param right
+   */
   void setRight(float right){
     centerX = right - w/2;
   }
+  
+  /** 
+   * @return float
+   */
   float getRight(){
     return centerX + w/2;
   }
+  
+  /** 
+   * @param top
+   */
   void setTop(float top){
     centerY = top + h/2;
   }
+  
+  /** 
+   * @return float
+   */
   float getTop(){
     return centerY - h/2;
   }
+  
+  /** 
+   * @param bottom
+   */
   void setBottom(float bottom){
     centerY = bottom - h/2;
   }
+  
+  /** 
+   * @return float
+   */
   float getBottom(){
     return centerY + h/2;
   }
 
+  
   //------------------ SPRITE IMAGE & ANIMATION METHODS --------------------//
 
-  //Accessor method to the Sprite object
+  /**
+   * Accesses the Sprite's image
+   * @return PImage
+   */
   public PImage getImage(){
     return this.spriteImg;
   }
-  //Mutator method to the Sprite object
+  
+  /** 
+   * Changes the Sprite's image
+   * @param img
+   */
   public void setImage(PImage img){
     this.spriteImg = img;
   }
-
-  //Accessor method to check if Sprite object is animated
+  
+  /** 
+   * Checks if Sprite object is animated
+   * @return boolean
+   */
   public boolean getIsAnimated(){
     return isAnimated;
   }
-  //Mutator method to change if Sprite object is animated
-  public void setIsAnimated(boolean a){
-    isAnimated = a;
+  
+  /** 
+   * Changes if Sprite object is animated
+   * @param isAnimated
+   */
+  public void setIsAnimated(boolean isAnimated){
+    this.isAnimated = isAnimated;
   }
-
-  //Accessor method to the image path of the Sprite
+  
+  /** 
+   * Gets the image path of the Sprite
+   * @return String
+   */
   public String getImagePath(){
     return this.spriteImgFile;
   }
 
-  //Method to be used to compare 2 sprites by a name, will check the image file name if no name specified
+  /** 
+   * Compares 2 sprites by a name, will check the image file name if no name specified
+   * @return String
+   */
   public String getName(){
     if(name == null){
       return getImagePath();
@@ -371,14 +571,21 @@ public class Sprite{
       return name;
     }
   }
-
-  //Sets the Sprites name to be used for comparisons
+  
+  /** 
+   * Sets the Sprites name to be used for comparisons
+   * @param name
+   */
   public void setName(String name){
     this.name = name;
   }
 
-
-  //Method to copy a Sprite to a specific location
+  /** 
+   * Copies a Sprite to a specific location
+   * @param x
+   * @param y
+   * @return Sprite
+   */
   public Sprite copyTo(float x, float y){
 
     // PImage si = this.spriteImg;
@@ -397,19 +604,49 @@ public class Sprite{
     sp.setH(h);
 
     return sp;
-
   }
 
+  
+  /** 
+   * Copies a new Sprite to same location as this Sprite
+   * @return Sprite
+   */
+  public Sprite copy(){
+    return copyTo(this.centerX, this.centerY);
+  }
+
+  /** 
+   * Scales the Sprite's image size to a new width & height
+   * @param width           how many pixels wide the Sprite will change to
+   * @param height          how many pixels high the Sprite will change to
+   */
   public void resize(int width, int height){
     spriteImg.resize(width, height);
   }
+  
+  /** 
+   * Checks if this Sprite is same as otherSprite (based on name or image)
+   * @param otherSprite   second Sprite to compare this Sprite with
+   * @return boolean      true if both Sprites have the same name or image, false otherwise
+   */
+  public boolean equals(Sprite otherSprite){
+    if(this.spriteImgFile != null && otherSprite != null && this.getName().equals(otherSprite.getName())){
+      return true;
+    }
+    return false;
+  }
 
-
-  // // method that automatically moves the Sprite based on its velocity
+  /**
+   * Automatically moves the Sprite based on its velocity
+   */
   public void update(){
     move(speedX, speedY);
   }
 
+  /** 
+   * Method called to update a Sprite's position based on its set speed and acceleration
+   * @param deltaTime
+   */
   public void update(float deltaTime){
 
     // determine number of seconds since last run of update
@@ -429,19 +666,9 @@ public class Sprite{
     update();
   }
 
-  //Method to copy a Sprite to same location
-  public Sprite copy(){
-    return copyTo(this.centerX, this.centerY);
-  }
-
-  //Method to check if 2 Sprites are the same (based on name or image)
-  public boolean equals(Sprite otherSprite){
-    if(this.spriteImgFile != null && otherSprite != null && this.getName().equals(otherSprite.getName())){
-      return true;
-    }
-    return false;
-  }
-
+  /** 
+   * @return String       includes Sprite's filename, centerX, centerY, left edge, top edge, speedX, speedY, w, h, isAnimated
+   */
   public String toString(){
     return "Sprite: fn: "+ spriteImgFile + "\tx:" + centerX + "\ty:" + centerY + "\tL:" + getLeft() + "\tT:" + getTop() + "\tVx:" + speedX + "\tVy:" + speedY + "\tw:" + w + "\th:" + h + "\t" + isAnimated;
   }
